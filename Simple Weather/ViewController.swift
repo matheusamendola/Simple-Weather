b3397bd2b7286ffddc80eb09c968d6d8e68d5c5b
Matheus Amendola <matheusamendolaa@gmail.com>
2020-07-05 13:13:30 -0300

    Initial Commit

---
+++ Simple Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Weather
+//
+//  Created by Matheus Amendola on 05/07/20.
+//  Copyright © 2020 Matheus Amendola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
